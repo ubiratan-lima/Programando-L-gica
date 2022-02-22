c7c832f0a8ad2d145b88907693fa2c1e32f4d421
ubiratan-lima <uslima05@gmail.com>
2022-02-22 15:27:30 -0300

    commit.webinarteste1

---
+++ UC8.Encontro 1.js
@@ -0,0 +1,9 @@
+var listaDePeças=["Amortecedor", "Motor", "Filtro do Ar"]
+
+let peso=200
+if(peso<100) {
+console("A peça deve pesar no minimo 100g")
+} else {
+console.log ("A peça tem o peso adequado")
+}
+
